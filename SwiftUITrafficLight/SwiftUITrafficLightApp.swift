babf8fc8a102b9c85b459a37022f5de6026cf4f8
ISkvor <starlingy608@gmail.com>
2022-04-19 18:30:46 +0300

    Initial Commit

---
+++ SwiftUITrafficLight/SwiftUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp.swift
+//  SwiftUITrafficLight
+//
+//  Created by Юрий Скворцов on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
